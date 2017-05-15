4b2556db2d0ef5941e87739e0d26d7ef6a40ad35
Vincent Lloyd <vincent.lloyd@protonmail.com>
2017-05-15 12:06:45 +1000

    Squad group functionality addedd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,19 @@ class HomeController < ApplicationController
   def index
     @captain_profile = current_user.captain_profile
     if @captain_profile.present?
-      @results = SquadGroup.where(captain_id: current_user.id)
+      @captain_results = SquadGroup.where(captain_id: current_user.id)
     end
+
+    @member_profile = SquadMember.where(user_id: current_user.id)
+    @squad_groups = []
+    if @member_profile.present?
+      @member_profile.each do |member|
+        @squad_groups.push(SquadGroup.find(member.squadgroup_id))
+      end
+      # @member_results = SquadGroup.where(captain_id: current_user.id)
+    end
+    puts '=' *50
+    pp @squad_groups
+    puts '=' *50
   end
 end
